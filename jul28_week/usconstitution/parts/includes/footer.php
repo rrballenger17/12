0dc9eb4d44faa815e7f6ba1d7b8281e849ae4122
rrballenger17 <rrb@alumni.stanford.edu>
2016-07-22 18:56:45 -0400

    Add files via upload

---
+++ jul28_week/usconstitution/parts/includes/footer.php
@@ -0,0 +1,8 @@
+
+<footer role="contentinfo">
+<?php echo "This page was last modified: " . date ("F j o", getlastmod());?>
+<br>
+<?php echo "URI: http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
+<p>This version of the United States Constitution was adapted from <a href="http://www.house.gov/">U.S. House of Representatives</a> website.  <a href="http://www.archives.gov/exhibits/charters/constitution.html">Learn more about the United States Constitution at <strong>The National Archives</strong> online exhibit</a>.</p>
+</footer>
+
---
+++ jul28_week/usconstitution/parts/includes/navigation.php
@@ -0,0 +1,13 @@
+
+<nav role="navigation">
+    <ul>
+      <li id="one"><a href="index.php">Article I</a></li>
+      <li id="two"><a href="ii.php">Article II</a></li>
+      <li id="three"><a href="iii.php">Article III</a></li>
+      <li id="four"><a href="iv.php">Article IV</a></li>
+      <li id="five"><a href="v.php">Article V</a></li>
+      <li id="six"><a href="vi.php">Article VI</a></li>
+      <li id="seven"><a href="vii.php">Article VII</a></li>
+      <li id="amendments"><a href="amendments.php">Amendments</a></li>
+    </ul>
+</nav>
